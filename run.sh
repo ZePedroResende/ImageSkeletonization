3727d0e24de7323e08713537c8bd19c59b50b396
a-sac <asergioalvesc@gmail.com>
2018-11-12 18:24:15 +0000

    testes & parallel for

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module load gcc/5.3.0
-cd $HOME/dev/PCP/
-make clean && make
-./bin/skeleton_seq haskell.pgm
+cd $HOME/CPD/PCP/Trabalho
+make clean
+make
+./bin/skeleton_seq images/${file}.pbm
